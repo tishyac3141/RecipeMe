ef1414d6accab5b8868f6295ddd136fe9ec46a83
Tishya Chhabra <tatooine888@gmail.com>
2020-05-29 10:19:58 -0700

    edited getLink

--- recipe_me/python-backend/getLink.py
+++ recipe_me/python-backend/getLink.py
@@ -1,4 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+def run(list):
+    for i in list:
+        URL = list[i]
+        page = requests.get(URL)
+
+        soup = BeautifulSoup(page.content, 'html.parser')
 

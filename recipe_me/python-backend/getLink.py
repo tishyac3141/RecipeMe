e0197a494a158fbdc3ab83d4ee684a1b71314bf6
Tishya Chhabra <tatooine888@gmail.com>
2020-05-29 10:58:24 -0700

    finished basics of getLink

--- recipe_me/python-backend/getLink.py
+++ recipe_me/python-backend/getLink.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import requests
 
 def run(list):
+    driver = webdriver.Chrome()
     for i in list:
         URL = list[i]
-        page = requests.get(URL)
-
-        soup = BeautifulSoup(page.content, 'html.parser')
+        driver.get(URL)
+        # need to get preferences from firebase to search within the text
+        if(driver.getPageSource().contains("hello")):
+            list.remove[i]
 

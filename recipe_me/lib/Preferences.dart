98d4461208022fbce681579cd741f92d61497de2
Tishya Chhabra <tatooine888@gmail.com>
2020-05-30 14:58:14 -0700

    edited preferences

--- recipe_me/lib/Preferences.dart
+++ recipe_me/lib/Preferences.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Preferences extends StatefulWidget{
  
-   Preferences({Key key, this.title, this.uid}) : super(key: key); 
+   Preferences({Key key, this.title}) : super(key: key); 
   //update the constructor to include the uid
   final String title;
-  final String uid;
 
   @override
   State<StatefulWidget> createState() {

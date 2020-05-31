fce2873425c9d8730fefb0d776292c9acef2a7f9
Tishya Chhabra <tatooine888@gmail.com>
2020-05-30 17:54:25 -0700

    added python backend

---
+++ recipe_me/python_backend/getLink.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+def run(list):
+    driver = webdriver.Chrome()
+    for i in list:
+        URL = list[i]
+        driver.get(URL)
+        # need to get preferences from firebase to search within the text
+        if(driver.getPageSource().contains("hello")):
+            list.remove[i]
+
+    return list
+
+
